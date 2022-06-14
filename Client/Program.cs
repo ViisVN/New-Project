2e498bf5b0b00aa4d32ccabb5ddc83b9ab5a8c0a
Huynh Nhat Hao <bim.bim3999@gmail.com>
2022-06-14 11:23:18 +0700

    First commit

---
+++ Client/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            Console.Write("Message: ");
+            string msg = Console.ReadLine() ?? "";
+            msg = msg.Equals("") ? "UDP Demp" : msg;
+            byte[] dataToEncrypt = ByteConverter.GetBytes(msg);
+            byte[] encryptedData;
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+            }
+            byte[] data = encryptedData;
+            string ipAddress = "127.0.0.1";
+            int sendPort = 2021;
+            try
+            {
+                using (var client = new UdpClient())
+                {
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                    client.Connect(ep);
+                    client.Send(data, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo); encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString()); return null;
+            }
+        }
+
+
+
+    }
+}
